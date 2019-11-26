6bada625496e5344c73192731c8d5d7c252f61d9
Hitoki <diavolos1230@gmail.com>
2019-11-26 21:48:00 +0900

    8.5割完了！

--- app/src/main/java/jp/techacademy/ito/hiroki/taskapp/MainActivity.kt
+++ app/src/main/java/jp/techacademy/ito/hiroki/taskapp/MainActivity.kt
@@ -10,13 +10,18 @@ import android.content.Intent
 import android.support.v7.app.AlertDialog
 import android.app.AlarmManager
 import android.app.PendingIntent
-import android.widget.SearchView
+import android.support.v7.widget.SearchView
 import android.util.Log
+import android.support.v7.widget.SearchView.OnQueryTextListener
+import android.view.Menu
+import android.widget.Toolbar
 
 const val EXTRA_TASK = "jp.techacademy.taro.kirameki.taskapp.TASK"
 
 class MainActivity : AppCompatActivity() {
 
+    var countries: MutableList<String> = ArrayList()
+    var displayList: MutableList<String> = ArrayList()
 
     private lateinit var mRealm: Realm
     private val mRealmListener = object : RealmChangeListener<Realm> {
@@ -25,115 +30,115 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-        private lateinit var mTaskAdapter: TaskAdapter
+    private lateinit var mTaskAdapter: TaskAdapter
 
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-            setContentView(R.layout.activity_main)
+    override fun onCreate(savedInstanceState: Bundle?) {
 
-            fab.setOnClickListener { view ->
-                val intent = Intent(this@MainActivity, InputActivity::class.java)
-                startActivity(intent)
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        toolbar.title = "検索"
+        setActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            val intent = Intent(this@MainActivity, InputActivity::class.java)
+            startActivity(intent)
+        }
+
+
+        // Realmの設定
+        mRealm = Realm.getDefaultInstance()
+        mRealm.addChangeListener(mRealmListener)
+
+        // ListViewの設定
+        mTaskAdapter = TaskAdapter(this@MainActivity)
+
+        // ListViewをタップしたときの処理
+        listView1.setOnItemClickListener { parent, _, position, _ ->
+            // 入力・編集する画面に遷移させる
+            val task = parent.adapter.getItem(position) as Task
+            val intent = Intent(this@MainActivity, InputActivity::class.java)
+            intent.putExtra(EXTRA_TASK, task.id)
+            startActivity(intent)
+        }
+
+        // ListViewを長押ししたときの処理
+        listView1.setOnItemLongClickListener { parent, _, position, _ ->
+            // タスクを削除する
+            val task = parent.adapter.getItem(position) as Task
+
+            // ダイアログを表示する
+            val builder = AlertDialog.Builder(this@MainActivity)
+
+            builder.setTitle("削除")
+            builder.setMessage(task.title + "を削除しますか")
+
+            builder.setPositiveButton("OK") { _, _ ->
+                val results = mRealm.where(Task::class.java).equalTo("id", task.id).findAll()
+
+                mRealm.beginTransaction()
+                results.deleteAllFromRealm()
+                mRealm.commitTransaction()
+
+                val resultIntent = Intent(applicationContext, TaskAlarmReceiver::class.java)
+                val resultPendingIntent = PendingIntent.getBroadcast(
+                    this@MainActivity,
+                    task.id,
+                    resultIntent,
+                    PendingIntent.FLAG_UPDATE_CURRENT
+                )
+
+                val alarmManager = getSystemService(ALARM_SERVICE) as AlarmManager
+                alarmManager.cancel(resultPendingIntent)
+
+                reloadListView()
             }
 
-            // Realmの設定
-            mRealm = Realm.getDefaultInstance()
-            mRealm.addChangeListener(mRealmListener)
+            builder.setNegativeButton("CANCEL", null)
 
-            // ListViewの設定
-            mTaskAdapter = TaskAdapter(this@MainActivity)
+            val dialog = builder.create()
+            dialog.show()
 
-            // ListViewをタップしたときの処理
-            listView1.setOnItemClickListener { parent, _, position, _ ->
-                // 入力・編集する画面に遷移させる
-                val task = parent.adapter.getItem(position) as Task
-                val intent = Intent(this@MainActivity, InputActivity::class.java)
-                intent.putExtra(EXTRA_TASK, task.id)
-                startActivity(intent)
-            }
+            true
+        }
 
-            // ListViewを長押ししたときの処理
-            listView1.setOnItemLongClickListener { parent, _, position, _ ->
-                // タスクを削除する
-                val task = parent.adapter.getItem(position) as Task
+        reloadListView()
+    }
 
-                // ダイアログを表示する
-                val builder = AlertDialog.Builder(this@MainActivity)
+    private fun reloadListView() {
+        // Realmデータベースから、「全てのデータを取得して新しい日時順に並べた結果」を取得
+        val taskRealmResults =
+            mRealm.where(Task::class.java).findAll().sort("date", Sort.DESCENDING)
 
-                builder.setTitle("削除")
-                builder.setMessage(task.title + "を削除しますか")
+        // 上記の結果を、TaskList としてセットする
+        mTaskAdapter.taskList = mRealm.copyFromRealm(taskRealmResults)
 
-                builder.setPositiveButton("OK") { _, _ ->
-                    val results = mRealm.where(Task::class.java).equalTo("id", task.id).findAll()
+        // TaskのListView用のアダプタに渡す
+        listView1.adapter = mTaskAdapter
 
-                    mRealm.beginTransaction()
-                    results.deleteAllFromRealm()
-                    mRealm.commitTransaction()
+        // 表示を更新するために、アダプターにデータが変更されたことを知らせる
+        mTaskAdapter.notifyDataSetChanged()
+    }
 
-                    val resultIntent = Intent(applicationContext, TaskAlarmReceiver::class.java)
-                    val resultPendingIntent = PendingIntent.getBroadcast(
-                        this@MainActivity,
-                        task.id,
-                        resultIntent,
-                        PendingIntent.FLAG_UPDATE_CURRENT
-                    )
-
-                    val alarmManager = getSystemService(ALARM_SERVICE) as AlarmManager
-                    alarmManager.cancel(resultPendingIntent)
-
-                    reloadListView()
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.search, menu)
+        if (menu != null) {
+            val searchView = menu.findItem(R.id.menu_search).actionView as SearchView?
+            searchView!!.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+                override fun onQueryTextSubmit(text: String?): Boolean {
+                    Log.d("kotlintest", "submit text: $text") //reloadlistviewの変更処理を記述データ
+                    return false
                 }
 
-                builder.setNegativeButton("CANCEL", null)
-
-                val dialog = builder.create()
-                dialog.show()
-
-                true
-            }
-
-            reloadListView()
+                override fun onQueryTextChange(text: String?): Boolean {
+                    Log.d("kotlintest", "change text: $text") //reloadlistviewの変更処理を記述
+                    return false
+                }
+            })
         }
-
-        private fun reloadListView() {
-            // Realmデータベースから、「全てのデータを取得して新しい日時順に並べた結果」を取得
-            val taskRealmResults =
-                mRealm.where(Task::class.java).findAll().sort("date", Sort.DESCENDING)
-
-            // 上記の結果を、TaskList としてセットする
-            mTaskAdapter.taskList = mRealm.copyFromRealm(taskRealmResults)
-
-            // TaskのListView用のアダプタに渡す
-            listView1.adapter = mTaskAdapter
-
-            // 表示を更新するために、アダプターにデータが変更されたことを知らせる
-            mTaskAdapter.notifyDataSetChanged()
-        }
-
-            menuInflater.inflate(R.menu.search, menu)
-            if (menu != null) {
-                val searchView = menu.findItem(R.id.menu_search).actionView as SearchView
-                searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
-                    override fun onQueryTextSubmit(text: String?): Boolean {
-                        // 検索キーが押下された
-                        Log.d(TAG, "submit text: $text")
-                        return false
-                    }
-
-                    override fun onQueryTextChange(text: String?): Boolean {
-                        // テキストが変更された
-                        Log.d(TAG, "change text: $text")
-                        val itemListFragment = fragmentManager.findFragmentById(R.id.container)
-                        if (itemListFragment is ItemListFragment && text != null) {
-                            itemListFragment.searchRequest(text)
-                        }
-                        return false
-                    }
-
-                })
-            }
-            return super.onCreateOptionsMenu(menu)
-        }
+        return super.onCreateOptionsMenu(menu)
+    }
 
         override fun onDestroy() {
             super.onDestroy()
